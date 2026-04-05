396f4700f0b0d04517fdbbf175b3cb9408a9d722
Srujan kumar <srujan.psj@gmai.com>
2026-04-05 12:17:26 +0530

    New conversation added

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=RAMESH
 PERSON2=SURESH
 
-echo "$PERSON1 :: Hi PERSON2"
-echo "PERSON2 :: Hello $PERSON1"
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
 echo "$PERSON1 :: How are you doing?"
-echo "PERSON2 :: I am good "
+echo "$PERSON2 :: I am good "

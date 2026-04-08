bc62389222748ca82fbd3a76b921c510ff9a7736
Srujan kumar <srujan.psj@gmai.com>
2026-04-08 05:39:03 +0530

    sum of 2 numbers

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+movies=("pushpa","rrr","devara")
+
+echo "First movie is ${movies[0]}"
+echo "Second movie is ${movies[1]}"
+echo "Third movie is ${movies[2]}"
+
+echo "All movies are ${movies[@]}"
+
+

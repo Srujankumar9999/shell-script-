c6c71945906ed05517e72b6b91080e9a868c763e
Srujan kumar <srujan.psj@gmai.com>
2026-04-08 06:19:07 +0530

    condition

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,7 +4,7 @@ Number=$1
 
 if [ $Number -gt 100]
 then 
-echo"Given number is greater that 100"
+    echo "Given number is greater that 100"
 else
-echo"Given number is less than 100"
+    echo "Given number is less than 100"
 fi

e54d9faad1edb2c20fb10c22126d18acdd9558dd
Srujan kumar <srujan.psj@gmai.com>
2026-04-08 05:43:44 +0530

    Array list

--- 06-array.sh
+++ 06-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 movies=("pushpa","rrr","devara")
 
-echo "First movie is ${movies[0]}"
-echo "Second movie is ${movies[1]}"
-echo "Third movie is ${movies[2]}"
+echo "First movie is : ${movies[0]}"
+echo "Second movie is : ${movies[1]}"
+echo "Third movie is : ${movies[2]}"
 
 echo "All movies are ${movies[@]}"
 
